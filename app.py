629280bb62e08a6953e96a4bdf5471f68ecfa88f
Dean Shaff <dean.shaff@gmail.com>
2018-10-30 22:43:46 -0500

    changed app to create a new observer object everytime data is requested

--- app.py
+++ app.py
@@ -31,26 +31,10 @@ def sign(n):
 
 class App(object):
 
-    # planets_info = {
-    #     'jupiter': {'ephem_obj': ephem.Jupiter(), 'color': "rgba(150,81,46,{})", 'size': 1.0},
-    #     'venus': {'ephem_obj': ephem.Venus(), 'color': "rgba(171,99,19,{})", 'size': 1.0},
-    #     'saturn': {'ephem_obj': ephem.Saturn(), 'color': "rgba(215,179,119,{})", 'size': 1.0},
-    #     'mars': {'ephem_obj': ephem.Mars(), 'color': "rgba(114,47,18,{})", 'size': 1.0},
-    #     'mercury': {'ephem_obj': ephem.Mercury(), 'color': "rgba(215,179,119,{})", 'size': 1.0},
-    #     'uranus': {'ephem_obj': ephem.Uranus(), 'color': "rgba(195,233,236,{})", 'size': 1.0},
-    #     'neptune': {'ephem_obj': ephem.Neptune(), 'color': "rgba(71,114,255,{})", 'size': 1.0},
-    #     'sun': {'ephem_obj': ephem.Sun(), 'color': "rgba(255,204,0,{})", 'size': 2.0},
-    #     'moon': {'ephem_obj': ephem.Moon(), 'color': "rgba(128,128,128,{})", 'size': 1.0}
-    # }
-
     socket_handlers = ["get_astron_object_data", "disconnect", "connect"]
     routes = {"main": "/"}
 
     def __init__(self):
-
-        observer = ephem.Observer()
-        observer.pressure = 0
-        observer.epoch = ephem.J2000
 
         app = Flask(
             __name__,
@@ -60,7 +44,6 @@ class App(object):
         app.config["SECRET_KEY"] = "planet-app"
         socketio = SocketIO(app)
 
-        self.observer = observer
         self.app = app
         self.socketio = socketio
         self.n_connections = 0
@@ -68,6 +51,13 @@ class App(object):
 
         self.generate_routes()
         self.generate_handlers()
+
+    def init_observer(self):
+
+        observer = ephem.Observer()
+        observer.pressure = 0
+        observer.epoch = ephem.J2000
+        return observer
 
     def get_astron_object_data(self, data):
         """
@@ -81,21 +71,22 @@ class App(object):
         Returns:
             None
         """
+        observer = self.init_observer()
         observer_location = data["geo_location"]
-        self.observer.lon = str(observer_location["lon"])
-        self.observer.lat = str(observer_location["lat"])
-        self.observer.elevation = float(observer_location["elevation"])
+        observer.lon = str(observer_location["lon"])
+        observer.lat = str(observer_location["lat"])
+        observer.elevation = float(observer_location["elevation"])
         when_str = str(data["when"])
         when = datetime.datetime.strptime(
             when_str,
             "%Y-%m-%dT%H:%M:%SZ"
         )
-        self.observer.date = when
+        observer.date = when
         astron_obj_name = str(data["name"])
 
         astron_obj = getattr(ephem, astron_obj_name)()
         self.app.logger.debug("get_astron_object_data: {}".format(astron_obj))
-        astron_obj.compute(self.observer)
+        astron_obj.compute(observer)
 
         return_data = {
             "name": astron_obj_name,
@@ -105,87 +96,12 @@ class App(object):
             "el": astron_obj.alt,
             "ra": astron_obj.ra,
             "dec": astron_obj.dec,
-            "setting_time": str(self.observer.next_setting(astron_obj)),
+            "setting_time": str(observer.next_setting(astron_obj)),
             "when": when_str
         }
         cb_name = data["cb_name"]
         with self.app.app_context():
             self.socketio.emit(cb_name, return_data)
-
-    # def get_planets(self, data):
-    #     """
-    #     Args:
-    #         current_position (dict): A dictionary containing the current position of the observer.
-    #     Returns:
-    #         dict: Containing az/alt information about the 7 non earth planets.
-    #             The az/alt information is in radians.
-    #     """
-    #     self.observer.lon = str(data['kwargs']['pos']['lon'])
-    #     self.observer.lat = str(data['kwargs']['pos']['lat'])
-    #     self.observer.elevation = data['kwargs']['pos']['elevation']
-    #     self.app.logger.debug("Lat: {}, Lon: {}".format(self.observer.lat, self.observer.lon))
-    #     continuous = False
-    #     cb_info = data['kwargs']['cb_info']
-    #     cb = cb_info['cb']
-    #     self.app.logger.debug("cb name: {}".format(cb))
-    #     if continuous:
-    #         self.start_time += datetime.timedelta(minutes=15)
-    #         self.observer.date = self.start_time
-    #         utcnow = self.start_time
-    #     else:
-    #         utcnow = datetime.datetime.utcnow()
-    #         self.observer.date = utcnow
-    #
-    #     t0 = time.time()
-    #     planet_list = []
-    #
-    #     for pl_key in self.planets_info:
-    #         ti = time.time()
-    #         planet = self.planets_info[pl_key]
-    #         ephem_obj = planet['ephem_obj']
-    #         same_day_position = [] # the position of the planet on the same day, throughout the day. (For drawing arcs)
-    #         same_time_position = [] # the position of the planet at the same time, but at different days
-    #         time_var = utcnow
-    #         for i in range(28): # a month
-    #             time_var += datetime.timedelta(days=1)
-    #             self.observer.date = time_var
-    #             ephem_obj.compute(self.observer)
-    #             same_time_position.append([ephem_obj.az, ephem_obj.alt])
-    #
-    #         time_var = utcnow
-    #         self.observer.date = time_var
-    #         ephem_obj.compute(self.observer)
-    #         same_day_position.append([ephem_obj.az, ephem_obj.alt])
-    #         setting_time = None
-    #         while (True):
-    #             time_var += datetime.timedelta(minutes=15)
-    #             self.observer.date = time_var
-    #             ephem_obj.compute(self.observer)
-    #             if sign(ephem_obj.alt) != sign(same_day_position[-1][1]):
-    #                 setting_time = time_var
-    #                 break
-    #             else:
-    #                 same_day_position.append([ephem_obj.az, ephem_obj.alt])
-    #             # self.app.logger.debug("Current alt: {}".format(same_day_position[-1][1]))
-    #
-    #         # self.app.logger.debug("Took {:.2f} seconds to compute planet {}".format(time.time() - ti, pl_key))
-    #         ephem_obj.compute(self.observer)
-    #         # print(ephem_obj.mag)
-    #         # self.app.logger.debug("{}: AZ: {} ALT: {}\nRA: {} DEC: {}".format(pl_key, ephem_obj.az, ephem_obj.alt,
-    #         #                                                                 ephem_obj.ra, ephem_obj.dec))
-    #         planet_list.append({'setting_time': setting_time.strftime("%H:%M:%S"),
-    #                             'color': planet['color'],
-    #                             'name': pl_key.capitalize(),
-    #                             'size': math.log(ephem_obj.size),
-    #                             'sameDayPos': same_day_position,
-    #                             'sameTimePos': same_time_position,
-    #                             'magnitude': ephem_obj.mag})
-    #
-    #     self.app.logger.debug("Took {:.2f} seconds to compute planet positions".format(time.time() - t0))
-    #     with self.app.test_request_context("/"):
-    #         self.app.logger.debug("Requested context");
-    #         self.socketio.emit(cb, planet_list)
-    #         self.app.logger.debug("Fired callback");
 
     def connect(self):
         self.n_connections += 1
